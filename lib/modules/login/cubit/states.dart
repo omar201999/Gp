3d289b5af582e4cebdc9d0b313fd1e60832a36ae
Omar Essam <essamomar528@gmail.com>
2022-03-12 04:37:37 +0200

    omar_task

---
+++ lib/modules/login/cubit/states.dart
@@ -0,0 +1,32 @@
+abstract class LoginStates {}
+
+class LoginInitialState extends LoginStates{}
+
+class LoginChangePasswordVisibilityState extends LoginStates{}
+
+class LoginLoadingState extends LoginStates{}
+
+class LoginSuccessState extends LoginStates
+{
+  final String uId;
+
+  LoginSuccessState(this.uId);
+}
+
+class LoginErrorState extends LoginStates
+{
+  final String error;
+  LoginErrorState(this.error);
+}
+
+class GetUserDataLoadingState extends LoginStates{}
+
+class GetUserDataSuccessState extends LoginStates{}
+
+class GetUserDataErrorState extends LoginStates
+{
+  //final String error;
+  //GetUserDataErrorState(this.error);
+}
+
+

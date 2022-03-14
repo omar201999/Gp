280a512496748292b017291d8263a95a958e22a6
lenovo <menna.olama2011@gmail.com>
2022-03-14 15:05:49 +0200

    menna_task

---
+++ lib/layout/admin_layout/cubit/states.dart
@@ -0,0 +1,36 @@
+abstract class AdminStates {}
+
+class AdminInitialState extends AdminStates {}
+
+class ChangeBottomNavState extends AdminStates{}
+
+class AdminGetAllUsersLoadingState extends AdminStates {}
+
+class AdminGetAllUsersSuccessState extends AdminStates {}
+
+class AdminGetAllUsersErrorState extends AdminStates
+{
+  final String error;
+
+  AdminGetAllUsersErrorState(this.error);
+}
+
+class AdminDeleteUsersSuccessState extends AdminStates {}
+
+class AdminDeleteUsersErrorState extends AdminStates
+{
+  final String error;
+
+  AdminDeleteUsersErrorState(this.error);
+}
+
+//create recipe
+class CreateRecipeLoadingState extends AdminStates{}
+
+class CreateRecipeSuccessState extends AdminStates{}
+
+class CreateRecipeErrorState extends AdminStates{}
+
+class RecipeImagePickedSuccessState extends AdminStates{}
+
+class RecipeImagePickedErrorState extends AdminStates{}
--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gp/layout/admin_layout/admin_layout.dart';
+import 'package:gp/layout/admin_layout/cubit/cubit.dart';
 import 'package:gp/layout/home-layout/home_layout.dart';
 import 'package:gp/modules/login/login_screen.dart';
 import 'package:gp/shared/bloc_observer.dart';
 import 'package:gp/shared/componants/constant.dart';
 import 'package:gp/shared/network/local/cashe_helper.dart';
+import 'layout/home-layout/cubit/cubit.dart';
+import 'layout/home-layout/cubit/states.dart';
 import 'shared/styles/themes.dart';
 
 void main()
@@ -21,7 +24,7 @@ void main()
           uId = CacheHelper.getData(key: 'uId');
           if(uId == 'TQPBmSbjWGQcrDXqVzz8kW2Supg2')
           {
-            widget = AdminDashBored();
+            widget = AdminLayout();
             print(uId.toString());
           }
           else if(uId == null)
@@ -58,12 +61,23 @@ class MyApp extends StatelessWidget
   @override
   Widget build(BuildContext context)
   {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: lightTheme,
-      darkTheme: darkTheme ,
-      themeMode: ThemeMode.light,
-      home: startWidget,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => HomeCubit()..getUserData()),
+        BlocProvider(create: (context) => AdminCubit()),
+      ],
+      child: BlocConsumer<HomeCubit,HomeStates>(
+        listener:(context,state){} ,
+        builder: (context,state){
+          return  MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: lightTheme,
+            darkTheme: darkTheme ,
+            themeMode: ThemeMode.light,
+            home: startWidget,
+          );
+        },
+      ),
     );
   }
 

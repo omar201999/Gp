3d99d74b0a05afd1ae292c837216e6e4fb0d54dc
lenovo <menna.olama2011@gmail.com>
2022-03-19 22:50:59 +0200

    task_delete

--- lib/shared/componants/componants.dart
+++ lib/shared/componants/componants.dart
@@ -39,7 +39,7 @@ Widget defaultTextFormField({
   void Function(String)? onSubmitted,
   bool obscure = false,
   String? Function(String?)? validate,
-  //int? maxLines ,
+  int? maxLines = 1,
 
   //////////////////////////////////////
   String? label ,
@@ -57,7 +57,7 @@ Widget defaultTextFormField({
   onFieldSubmitted: onSubmitted,
   obscureText: obscure,
   validator: validate,
-  //maxLines: maxLines,
+  maxLines: maxLines,
 
   //decoration of textFormField
 

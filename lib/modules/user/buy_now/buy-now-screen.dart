773e401ce55f753dfa29ae6a63fd42e0d5f45ee2
essam <essamomar528@gmail.com>
2022-04-22 03:54:45 +0200

    update_done

--- lib/modules/user/buy_now/buy-now-screen.dart
+++ lib/modules/user/buy_now/buy-now-screen.dart
@@ -36,7 +36,7 @@ class BuyNowScreen extends StatelessWidget {
             foregroundColor: Colors.white,
             onPressed: ()
             {
-              if( HomeCubit.get(context).userModel!.address != null ||HomeCubit.get(context).userModel!.phone!= null  )
+              if( HomeCubit.get(context).userModel!.address != null && HomeCubit.get(context).userModel!.phone!= null && HomeCubit.get(context).userModel!.address !="" && HomeCubit.get(context).userModel!.phone!= ""  )
               {
                 HomeCubit.get(context).createOrder(total: HomeCubit.get(context).calculateTotalPriceOfCartItems()+100 , totalPrice: HomeCubit.get(context).calculateTotalPriceOfCartItems());
                 showToast(text: 'Your order done Successfully', state: ToastStates.SUCCESS);
